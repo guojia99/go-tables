070e32cb8bca76abae2274747b8866d7b71d3c56
guojia <guojia@guojia.com>
2023-03-27 16:44:15 +0800

    update copyright

---
+++ README.md
@@ -0,0 +1,2 @@
+# 测试版本, 请勿使用
+# Beta version, do not use
--- table/fotmat_test.go
+++ table/fotmat_test.go
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2023 guojia99 All rights reserved.
- * Created: 2023/2/24 下午10:01.
- * Author: guojia(zjguo@gizwits.com)
+ *  * Copyright (c) 2023 guojia99 All rights reserved.
+ *  * Created: 2023/2/26 下午5:22.
+ *  * Author: guojia(https://github.com/guojia99)
  */
 
 package table
