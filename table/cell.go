6d4a0c731858a76d45504d0d2433e500aaeee1af
guozejia <guozejia@bingosoft.net>
2024-10-24 16:20:44 +0800

    [20241024] 移除删除空格

--- table/cell.go
+++ table/cell.go
@@ -76,7 +76,7 @@ func (a Align) Repeat(in string, count uint) string {
 	if in == "" {
 		return strings.Repeat(" ", int(count))
 	}
-	in = strings.Replace(in, " ", "", -1)
+	//in = strings.Replace(in, " ", "", -1)
 	in = strings.Replace(in, "\n", "", -1)
 	in = strings.Replace(in, "\t", "", -1)
 	w := utils.RealLength(in)
